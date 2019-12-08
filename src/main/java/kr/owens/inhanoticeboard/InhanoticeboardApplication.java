841150b374c9fe07b8c50fb1cb012249799e7d98
owen151128 <owen151128@gmail.com>
2019-12-08 22:19:07 +0900

    Project default settings

---
+++ src/main/java/kr/owens/inhanoticeboard/InhanoticeboardApplication.java
@@ -0,0 +1,13 @@
+package kr.owens.inhanoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhanoticeboardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InhanoticeboardApplication.class, args);
+  }
+
+}
